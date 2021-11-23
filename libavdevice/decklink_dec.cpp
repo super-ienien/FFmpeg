6619033d2e89195b54f747e35ae1681f2e132867
Vivien <vivien.anglesio@gmail.com>
2021-11-23 20:11:37 +0100

    fix

--- libavdevice/decklink_dec.cpp
+++ libavdevice/decklink_dec.cpp
@@ -1260,12 +1260,11 @@ HRESULT decklink_input_callback::VideoInputFormatChanged(
         if (!cctx->raw_format)
             ctx->raw_format = (formatFlags & bmdDetectedVideoInputRGB444) ? bmdFormat8BitARGB : bmdFormat8BitYUV;
     } else {
-        BSTR modeNameBSTR;
-        mode->GetName(&modeNameBSTR);
-        _bstr_t modeName(modeNameBSTR);
+        const char* modeName = nullptr;
+        mode->GetName(&modeName);
 
         auto newMode = mode->GetDisplayMode();
-                av_log(avctx, AV_LOG_WARNING, "Decklink mode detection %s\n", (char*)modeName);
+                av_log(avctx, AV_LOG_WARNING, "Decklink mode detection %s\n", modeName);
         if (newMode != ctx->bmd_mode) {
             if (!wrong_mode) {
                 wrong_mode = 1;
